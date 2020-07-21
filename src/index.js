28a8866789166dea0aafcc0fb1dbc8ef2b6d7825
John Lindal <jafl@users.noreply.github.com>
2020-07-21 16:39:24 -0700

    Provide useful message and stack trace
    The stack trace in the original error just shows internal AWS SDK calls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const awsPromisify = (contex, func) => (params) => {
+  const e = new Error();
   return new Promise((resolve, reject) => {
     func.call(contex, params, (err, data) => {
       if (err) {
-        reject(err);
+        e.message = `${func.name} ${JSON.stringify(params[0])}: ${err.message}`;
+        reject(e);
       }
       if (data) {
         resolve(data);
